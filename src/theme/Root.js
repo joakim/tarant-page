624e088443b7efea55d039f3fd6a21eb59fcb1a9
kanekotic <me@kanekotic.com>
2023-03-16 15:49:50 +0100

    ALVARO | added auth

---
+++ src/theme/Root.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const privatePages = [""]
+
+// Default implementation, that you can customize
+export default function Root({children}) {
+    console.log(children)
+    return <Auth0Provider
+    domain="dev-u6zl0gyyil0lqmcm.eu.auth0.com"
+    clientId="xuMWdth6SsAXj7KYkJWZufVFUEfIE6Fh"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    {children}
+    </Auth0Provider>
+}
