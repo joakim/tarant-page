09d57a03e54ff6b661a73ab25782eb0b16af37df
kanekotic <me@kanekotic.com>
2023-03-22 09:44:26 +0100

    ALVARO | fix spacing

--- src/components/HomepageTryIt/index.tsx
+++ src/components/HomepageTryIt/index.tsx
@@ -3,6 +3,8 @@ import styles from './styles.module.css';
 
 export default function HomepageTryIt(): JSX.Element {
   return (
+    <section className={styles.features}>
         <iframe className={styles.codepen} title="tarant quick-start example" src="https://codepen.io/kmruiz/embed/xmLOqy?default-tab=js%2Cresult" loading="lazy" />
+    </section>
   );
 }
