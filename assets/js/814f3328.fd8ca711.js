2d5380a8417eb36490f1455f49aa51f722fa6c9e
kmruiz <kmruiz@users.noreply.github.com>
2023-04-17 17:09:10 +0000

    deploy: c11e48e195b07e27b597d45f3cbb7684f9e7ec54

---
+++ assets/js/814f3328.fd8ca711.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktarant_page=self.webpackChunktarant_page||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Tarant Cloud: Talk, Communicate and Solve","permalink":"/blog/2023/04/17/why-the-cloud"},{"title":"Website Revamp","permalink":"/blog/2023/03/12/website-revamp"}]}')}}]);
---
+++ assets/js/f3543915.721351c6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktarant_page=self.webpackChunktarant_page||[]).push([[3376],{7698:a=>{a.exports=JSON.parse('{"label":"cloud","permalink":"/blog/tags/cloud","allTagsPath":"/blog/tags","count":1}')}}]);
