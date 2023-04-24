44afc9ed726924d429c051da083c5400badcc6fb
kanekotic <kanekotic@users.noreply.github.com>
2023-04-24 18:57:08 +0000

    deploy: 2a6af455c4f64101070fc5d7430e67e13b8dbdec

---
+++ assets/js/f3eeb784.8e3f3015.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktarant_page=self.webpackChunktarant_page||[]).push([[8905],{1648:a=>{a.exports=JSON.parse('{"label":"opensource","permalink":"/blog/tags/opensource","allTagsPath":"/blog/tags","count":1}')}}]);
